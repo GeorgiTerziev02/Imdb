c9a72643230ba20906d8bdf81ae622de880da000
Georgi Terziev <terzata2002@abv.bg>
2020-04-16 13:40:08 +0300

    Move search to nav bar, create _SearchBarPartial

--- Web/Imdb.Web.ViewModels/Home/TopMovieViewModel.cs
+++ Web/Imdb.Web.ViewModels/Home/TopMovieViewModel.cs
@@ -2,19 +2,23 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Net;
     using System.Text;
     using System.Text.RegularExpressions;
+    using AutoMapper;
     using Imdb.Data.Models;
     using Imdb.Services.Mapping;
 
-    public class TopMovieViewModel : IMapFrom<Movie>
+    public class TopMovieViewModel : IMapFrom<Movie>, IHaveCustomMappings
     {
         public string Id { get; set; }
 
         public string Title { get; set; }
 
         public string Description { get; set; }
+
+        public double Rating { get; set; }
 
         public string ShortDescription
         {
@@ -28,5 +32,11 @@
         }
 
         public string GeneralImageUrl { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Movie, TopMovieViewModel>()
+                .ForMember(x => x.Rating, y => y.MapFrom(x => x.Votes.Average(x => x.Rating)));
+        }
     }
 }
